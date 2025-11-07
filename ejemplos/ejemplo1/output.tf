9d478eafc7d200ce3cff56b6acb777aa3affc13f
xs3rgii <roales.sergio04@gmail.com>
2025-11-07 13:02:37 +0100

    Subir el escenario a github sin ningun cambio

--- ejemplos/ejemplo1/output.tf
+++
@@ -1,6 +0,0 @@
-output "server1" {
-  value = {
-    nombre = "server1"
-    ip     = try(libvirt_domain.server1.network_interface[0].addresses[0], "No disponible")
-  }
-}
--- ejemplos/ejemplo3/output.tf
+++
@@ -1,7 +0,0 @@
-output "server1" {
-  value = {
-    nombre = "server1"
-    ip1    = try(libvirt_domain.server1.network_interface[0].addresses[0], "No disponible")
-    ip2    = try(libvirt_domain.server1.network_interface[1].addresses[0], "No disponible")
-  }
-}
