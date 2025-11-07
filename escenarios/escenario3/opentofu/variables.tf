72641dafb81b0d87fd7c63c9a5a7d2d06ac409e4
xs3rgii <roales.sergio04@gmail.com>
2025-11-07 14:16:03 +0100

    Escenario3 terminado de configurar previo a ajustes extras

--- escenarios/escenario3/opentofu/variables.tf
+++ escenarios/escenario3/opentofu/variables.tf
@@ -14,5 +14,5 @@ variable "libvirt_pool_name" {
 variable "libvirt_pool_path" {
   type        = string
   description = "Ruta local del directorio asociado al pool de almacenamiento libvirt."
-  default     = "/home/jose/vm"
+  default     = "/var/lib/libvirt/images"
 }
